0a0ce609d6e37f1d03db18fd7ffeb293dfc5ccb2
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-12-06 12:45:30 -0500

    Funciona correctamente, entendido todo

--- conteo.c
+++ conteo.c
@@ -9,33 +9,30 @@ Candidato 1, 2, 3, 4 y 5*/
 int main(int argc, char const *argv[])
 {
     int voto;
-    int candidatos[5] = {0};
-
     int ganador = 0;
+    int candidatos[5] = {0};
 
     do
     {
-        printf("Ingrese el numero de su candidato:\n");
+        printf("Ingrese el # del candidato para su voto y presione 0 para salir: \n");
         scanf("%d", &voto);
         candidatos[voto-1]++;
-    } while (voto != 0);
+
+    } while ( voto != 0);
 
     for (int i = 0; i < 5; i++)
     {
-        printf("Voto de candidato %d:\t %d\n", i+1, candidatos[i] );
-    }
-
-    for (int i = 0; i < 5; i++)
-    {
-        if (candidatos[i] > candidatos[ganador] )
+        printf("Los voto del candidato %d son %d.\n", i+1, candidatos[i]);
+        if (candidatos[i] > candidatos[ganador])
         {
-         ganador = i;
+            ganador = i;
         }
         
     }
-    printf("El candidato ganador es el %d con %d votos", ganador+1, candidatos[ganador]);
+
+    printf("El candidato ganador es %d con %d votos.\n", ganador+1, candidatos[ganador]);
     
     
-    
+
     return 0;
 }
